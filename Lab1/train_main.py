fd18f7bbff6b2556103e64f160c72fa90d6e4a87
JHan0220 <877653250@qq.com>
2024-03-27 22:38:00 -0700

    training

--- Lab1/train_main.py
+++ Lab1/train_main.py
@@ -1,3 +1,6 @@
+import string
+
+from Lab1.data_Gen import handwriting_recognition_data_generator
 from model import Model
 from activation import ActivationFunction
 from loss_function import LossFunction
@@ -13,14 +16,27 @@ def main():
     training_instance =Training(model, active, loss_function)
 
     # Load or generate your training data
-    X_train = ...  # Your training inputs
-    Y_train = ...  # Your training targets
+    # Initialize the data generator
+    data_generator = handwriting_recognition_data_generator(
+        chars=string.ascii_letters + string.digits,  # Characters to include
+        batch_size=32,  # Number of images/labels per batch
+        image_size=(28, 28),  # Size of the images
+        font_path='HappySwirly-KVB7l.ttf'  # Path to the font file
+    )
+
+
 
     # Define the number of epochs for training
     epochs = 1000  # Or any number you deem appropriate
 
+    for epoch in range(epochs):
+        # print(f"Epoch {epoch + 1}/{epochs}")
+        for step in range(epochs):
+            # Generate a batch of data
+            X_batch, Y_batch = next(data_generator)
+
     # Start training your model
-    training_instance.train(X_train, Y_train, epochs)
+    training_instance.train(X_batch, Y_batch, epochs)
 
 if __name__ == "__main__":
     main()
